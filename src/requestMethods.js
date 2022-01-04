1eaddf8b18d9a5439536866e3c35cacadae5579b
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-01-03 19:34:12 -0700

    update

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // const BASE_URL='https://swagapi.herokuapp.com/api/'
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).users).currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
